02df7ad0fce71fd2145ff277072ac146c243beb6
chen <chenkang40@huawei.com>
2024-08-27 10:13:17 +0800

    modify wasm load/store translate order
    Signed-off-by: chen <chenkang40@huawei.com>

--- src/compiler/wasm-compiler.h
+++ src/compiler/wasm-compiler.h
@@ -627,6 +627,9 @@ class WasmGraphBuilder {
   // offset fits in a platform-dependent uintptr_t.
   Node* MemBuffer(uintptr_t offset);
 
+  // only for load/store
+  Node* MemBuffer(Node* index);
+
   // BoundsCheckMem receives a 32/64-bit index (depending on
   // WasmModule::is_memory64) and returns a ptrsize index and information about
   // the kind of bounds check performed (or why none was needed).
