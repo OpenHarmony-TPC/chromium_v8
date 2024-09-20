04c0a2bb9517500caf7d3fe6bfd939807dfd9cc3
马宗富 <mazongfu1@h-partners.com>
2024-09-20 10:53:46 +0800

    origin/4.1_Release修复CVE-2023-3420
    Signed-off-by: 马宗富 <mazongfu1@h-partners.com>

--- src/compiler/machine-operator.cc
+++ src/compiler/machine-operator.cc
@@ -1443,7 +1443,6 @@ struct MachineOperatorGlobalCache {
   StackPointerGreaterThan##Kind##Operator kStackPointerGreaterThan##Kind;
 
   STACK_POINTER_GREATER_THAN(JSFunctionEntry)
-  STACK_POINTER_GREATER_THAN(JSIterationBody)
   STACK_POINTER_GREATER_THAN(CodeStubAssembler)
   STACK_POINTER_GREATER_THAN(Wasm)
 #undef STACK_POINTER_GREATER_THAN
@@ -1814,12 +1813,12 @@ const Operator* MachineOperatorBuilder::StackPointerGreaterThan(
   switch (kind) {
     case StackCheckKind::kJSFunctionEntry:
       return &cache_.kStackPointerGreaterThanJSFunctionEntry;
-    case StackCheckKind::kJSIterationBody:
-      return &cache_.kStackPointerGreaterThanJSIterationBody;
     case StackCheckKind::kCodeStubAssembler:
       return &cache_.kStackPointerGreaterThanCodeStubAssembler;
     case StackCheckKind::kWasm:
       return &cache_.kStackPointerGreaterThanWasm;
+    case StackCheckKind::kJSIterationBody:
+      UNREACHABLE();
   }
   UNREACHABLE();
 }
