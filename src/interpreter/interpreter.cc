c1eda8e9f7ca6c9b3630ea33ead726d4951bf699
Weili_Liu <liuweili1@huawei.com>
2024-10-09 03:07:32 +0000

    update src/interpreter/interpreter.cc.
    Signed-off-by: Weili_Liu <liuweili1@huawei.com>

--- src/interpreter/interpreter.cc
+++ src/interpreter/interpreter.cc
@@ -297,9 +297,6 @@ InterpreterCompilationJob::Status InterpreterCompilationJob::DoFinalizeJobImpl(
   }
 
 #ifdef DEBUG
-  if (parse_info()->literal()->shared_function_info().is_null()) {
-    parse_info()->literal()->set_shared_function_info(shared_info);
-  }
   CheckAndPrintBytecodeMismatch(
       isolate, handle(Script::cast(shared_info->script()), isolate), bytecodes);
 #endif
