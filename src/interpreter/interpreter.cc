0b08b341c25185f8e394aef083863c3521f3fbd3
qq_43425235 <2091711952@qq.com>
2025-06-28 14:58:10 +0800

    cve auto merge
    Signed-off-by: 2091711952 <2091711952@qq.com>

--- src/interpreter/interpreter.cc
+++ src/interpreter/interpreter.cc
@@ -292,6 +292,9 @@ InterpreterCompilationJob::Status InterpreterCompilationJob::DoFinalizeJobImpl(
   }
 
 #ifdef DEBUG
+  if (parse_info()->literal()->shared_function_info().is_null()) {
+    parse_info()->literal()->set_shared_function_info(shared_info);
+  }
   CheckAndPrintBytecodeMismatch(
       isolate, handle(Script::cast(shared_info->script()), isolate), bytecodes);
 #endif
