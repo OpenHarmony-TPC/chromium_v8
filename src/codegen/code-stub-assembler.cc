9b018e1ac8d4e005b534115e2860873a5b9dc61f
gongyadong <gongyadong@h-partners.com>
2025-11-18 14:04:57 +0800

    【安全】【平台通用】修复合入451355210，V8 Sandbox Bypass: AAW/PC control via OOB builtin in SharedFunctionInfo
    Signed-off-by: gongyadong <gongyadong@h-partners.com>
    Change-Id: I7bbfe92a03a402ff7c7abd9c043ad95def0a917f

--- src/codegen/code-stub-assembler.cc
+++ src/codegen/code-stub-assembler.cc
@@ -17036,7 +17036,8 @@ TNode<BoolT> CodeStubAssembler::NeedsAnyPromiseHooks(TNode<Uint32T> flags) {
 }
 
 TNode<Code> CodeStubAssembler::LoadBuiltin(TNode<Smi> builtin_id) {
-  CSA_DCHECK(this, SmiBelow(builtin_id, SmiConstant(Builtins::kBuiltinCount)));
+  CSA_SBXCHECK(this,
+               SmiBelow(builtin_id, SmiConstant(Builtins::kBuiltinCount)));
 
   TNode<IntPtrT> offset =
       ElementOffsetFromIndex(SmiToBInt(builtin_id), SYSTEM_POINTER_ELEMENTS);
@@ -17056,6 +17057,7 @@ TNode<JSDispatchHandleT> CodeStubAssembler::LoadBuiltinDispatchHandle(
 }
 #endif  // V8_ENABLE_LEAPTIERING
 
+// LINT.IfChange(GetSharedFunctionInfoCode)
 TNode<Code> CodeStubAssembler::GetSharedFunctionInfoCode(
     TNode<SharedFunctionInfo> shared_info, TVariable<Uint16T>* data_type_out,
     Label* if_compile_lazy) {
@@ -17214,6 +17216,7 @@ TNode<Code> CodeStubAssembler::GetSharedFunctionInfoCode(
   BIND(&done);
   return sfi_code.value();
 }
+// LINT.ThenChange(/src/objects/shared-function-info.cc:GetSharedFunctionInfoCode)
 
 TNode<RawPtrT> CodeStubAssembler::LoadCodeInstructionStart(
     TNode<Code> code, CodeEntrypointTag tag) {
