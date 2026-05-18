v8/v8

Reland "[wasm] Refactor storage of data segments in instance"
This is a reland of commit 35c1c729ffbe05a4faf329307b1aaeb0bad6f1f4.
PS2 implements `data.drop` via a C call which is allows to write to
trusted space.

Original change's description:
> [wasm] Refactor storage of data segments in instance
>
> Move from a `FixedAddressArray` plus `FixedUInt32Array` (stored in the
> sandbox) to a `TrustedPodArray<WireBytesRef>`.
> Storing absolute addresses in the sandbox is not a good idea anyway. The
> new design makes the instance smaller, keeps related data closer
> together and generally improves ergonomics when creating or using the
> `data_segments` array.
>
> Drive-by: Use a few more `base::Vector` to pass around views into the
> wire bytes.
>
> R=jkummerow@chromium.org
>
> Bug: 483092905
> Change-Id: I0a8d9b93b0d6c5f84eb6d09d9a6d42cc875da99b
> Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7572409
> Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
> Commit-Queue: Clemens Backes <clemensb@chromium.org>
> Cr-Commit-Position: refs/heads/main@{#105270}

Bug: 483092905
Change-Id: I9c20f2553e0b0f24dd82cc1941bdea1271fc35e4
Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7600437
Commit-Queue: Clemens Backes <clemensb@chromium.org>
Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
Cr-Commit-Position: refs/heads/main@{#105386}

test/common/wasm/wasm-run-utils.cc
@@ -378,10 +378,9 @@ uint32_t TestingModuleBuilder::AddException(const FunctionSig* sig) {
 
 uint32_t TestingModuleBuilder::AddPassiveDataSegment(
     base::Vector<const uint8_t> bytes) {
-  uint32_t index = static_cast<uint32_t>(module_->data_segments.size());
+  int index = static_cast<int>(module_->data_segments.size());
   DCHECK_EQ(index, module_->data_segments.size());
-  DCHECK_EQ(index, data_segment_starts_.size());
-  DCHECK_EQ(index, data_segment_sizes_.size());
+  DCHECK_EQ(index, trusted_instance_data_->data_segments()->length());
 
   // Add a passive data segment. This isn't used by function compilation, but
   // but it keeps the index in sync. The data segment's source will not be
@@ -392,35 +391,19 @@ uint32_t TestingModuleBuilder::AddPassiveDataSegment(
   // to validate the segment index, during function compilation.
   module_->num_declared_data_segments = index + 1;
 
-  Address old_data_address =
-      reinterpret_cast<Address>(data_segment_data_.data());
-  size_t old_data_size = data_segment_data_.size();
-  data_segment_data_.resize(old_data_size + bytes.size());
-  Address new_data_address =
-      reinterpret_cast<Address>(data_segment_data_.data());
-
-  memcpy(data_segment_data_.data() + old_data_size, bytes.begin(),
-         bytes.size());
-
-  // The data_segment_data_ offset may have moved, so update all the starts.
-  for (Address& start : data_segment_starts_) {
-    start += new_data_address - old_data_address;
+  DirectHandle<TrustedPodArray<WireBytesRef>> new_data_segments =
+      TrustedPodArray<WireBytesRef>::New(isolate_, index + 1);
+  for (int i = 0; i < index; ++i) {
+    new_data_segments->set(i, trusted_instance_data_->data_segments()->get(i));
   }
-  data_segment_starts_.push_back(new_data_address + old_data_size);
-  data_segment_sizes_.push_back(static_cast<uint32_t>(bytes.size()));
 
-  // The vector pointers may have moved, so update the instance object.
-  uint32_t size = static_cast<uint32_t>(data_segment_sizes_.size());
-  DirectHandle<FixedAddressArray> data_segment_starts =
-      FixedAddressArray::New(isolate_, size);
-  MemCopy(data_segment_starts->begin(), data_segment_starts_.data(),
-          size * sizeof(Address));
-  trusted_instance_data_->set_data_segment_starts(*data_segment_starts);
-  DirectHandle<FixedUInt32Array> data_segment_sizes =
-      FixedUInt32Array::New(isolate_, size);
-  MemCopy(data_segment_sizes->begin(), data_segment_sizes_.data(),
-          size * sizeof(uint32_t));
-  trusted_instance_data_->set_data_segment_sizes(*data_segment_sizes);
+  uint32_t new_segment_offset = AddBytes(bytes);
+  DCHECK_LE(bytes.size(), kMaxUInt32);
+  new_data_segments->set(
+      index,
+      WireBytesRef{new_segment_offset, static_cast<uint32_t>(bytes.size())});
+
+  trusted_instance_data_->set_data_segments(*new_data_segments);
   return index;
 }
 

OpenHarmony-TPC/chromium_v8

(cherry picked from commit e9f61f1bbcd8807817a75779b64f0381751c9f5c)
Signed-off-by: lgh-w <wulonghui2@huawei.com>

test/common/wasm/wasm-run-utils.cc
@@ -376,10 +376,9 @@ uint32_t TestingModuleBuilder::AddException(const FunctionSig* sig) {
 
 uint32_t TestingModuleBuilder::AddPassiveDataSegment(
     base::Vector<const uint8_t> bytes) {
-  uint32_t index = static_cast<uint32_t>(test_module_->data_segments.size());
+  int index = static_cast<int>(test_module_->data_segments.size());
   DCHECK_EQ(index, test_module_->data_segments.size());
-  DCHECK_EQ(index, data_segment_starts_.size());
-  DCHECK_EQ(index, data_segment_sizes_.size());
+  DCHECK_EQ(index, trusted_instance_data_->data_segments()->length());
 
   // Add a passive data segment. This isn't used by function compilation, but
   // but it keeps the index in sync. The data segment's source will not be
@@ -390,35 +389,19 @@ uint32_t TestingModuleBuilder::AddPassiveDataSegment(
   // to validate the segment index, during function compilation.
   test_module_->num_declared_data_segments = index + 1;
 
-  Address old_data_address =
-      reinterpret_cast<Address>(data_segment_data_.data());
-  size_t old_data_size = data_segment_data_.size();
-  data_segment_data_.resize(old_data_size + bytes.size());
-  Address new_data_address =
-      reinterpret_cast<Address>(data_segment_data_.data());
-
-  memcpy(data_segment_data_.data() + old_data_size, bytes.begin(),
-         bytes.size());
-
-  // The data_segment_data_ offset may have moved, so update all the starts.
-  for (Address& start : data_segment_starts_) {
-    start += new_data_address - old_data_address;
+  DirectHandle<TrustedPodArray<WireBytesRef>> new_data_segments =
+      TrustedPodArray<WireBytesRef>::New(isolate_, index + 1);
+  for (int i = 0; i < index; ++i) {
+    new_data_segments->set(i, trusted_instance_data_->data_segments()->get(i));
   }
-  data_segment_starts_.push_back(new_data_address + old_data_size);
-  data_segment_sizes_.push_back(static_cast<uint32_t>(bytes.size()));
 
-  // The vector pointers may have moved, so update the instance object.
-  uint32_t size = static_cast<uint32_t>(data_segment_sizes_.size());
-  DirectHandle<FixedAddressArray> data_segment_starts =
-      FixedAddressArray::New(isolate_, size);
-  MemCopy(data_segment_starts->begin(), data_segment_starts_.data(),
-          size * sizeof(Address));
-  trusted_instance_data_->set_data_segment_starts(*data_segment_starts);
-  DirectHandle<FixedUInt32Array> data_segment_sizes =
-      FixedUInt32Array::New(isolate_, size);
-  MemCopy(data_segment_sizes->begin(), data_segment_sizes_.data(),
-          size * sizeof(uint32_t));
-  trusted_instance_data_->set_data_segment_sizes(*data_segment_sizes);
+  uint32_t new_segment_offset = AddBytes(bytes);
+  DCHECK_LE(bytes.size(), kMaxUInt32);
+  new_data_segments->set(
+      index,
+      WireBytesRef{new_segment_offset, static_cast<uint32_t>(bytes.size())});
+
+  trusted_instance_data_->set_data_segments(*new_data_segments);
   return index;
 }
 